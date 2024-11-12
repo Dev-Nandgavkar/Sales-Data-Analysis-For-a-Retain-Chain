e66b219f72bee02d9cc845f8bd2c5c71066423dd
Dev-Nandgavkar <devnandgavkar@gmail.com>
2024-11-12 19:20:03 +0530

    Project Files

---
+++ Project SQL Queries.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE Project;
+USE Project;
+
+CREATE TABLE transactions(
+	customer_id VARCHAR(255),
+    trs_time DATETIME,
+    trs_amount INT
+);
+
+CREATE TABLE responses(
+	customer_id VARCHAR(255),
+    cst_response INT
+);
+
+SELECT * FROM transactions;
+SELECT * FROM responses;
+ALTER TABLE transactions MODIFY trs_time VARCHAR(255);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Retail_Data_Transactions.csv'
+INTO TABLE transactions
+FIELDS terminated by ','
+LINES terminated by '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Retail_Data_Response.csv'
+INTO TABLE responses
+FIELDS terminated by ','
+LINES terminated by '\n'
+IGNORE 1 ROWS;
